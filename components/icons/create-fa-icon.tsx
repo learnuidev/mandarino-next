10f8ba86d41578ee886a441ba7d2146c21993b6f
learnuidev <learnuidev@gmail.com>
2025-06-23 00:48:08 -0400

    fix any

--- components/icons/create-fa-icon.tsx
+++ components/icons/create-fa-icon.tsx
@@ -4,8 +4,7 @@ import {
 } from "@fortawesome/react-fontawesome";
 
 export const createFAIcon = (icon: FontAwesomeIconProps["icon"]) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return function FAIcon(props: any) {
+  return function FAIcon(props: Omit<FontAwesomeIconProps, "icon">) {
     return <FontAwesomeIcon icon={icon} {...props} />;
   };
 };
